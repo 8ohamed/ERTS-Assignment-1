30316d3bc426c10de96aa08929905a919680d8bc
8ohamed <114395272+8ohamed@users.noreply.github.com>
2025-09-11 22:22:34 +0200

    exercise3

---
+++ exercise3/TCP_2.cpp
@@ -0,0 +1,122 @@
+#include <systemc.h>
+#include <iostream>
+#include <random>
+#include <chrono>
+
+#define PACKET_SIZE 512
+#define DATA_SIZE (PACKET_SIZE - 20)
+
+struct TCPHeader {
+    sc_uint<16> SourcePort;
+    sc_uint<16> DestinationPort;
+    sc_uint<32> SequenceNumber;
+    sc_uint<32> Acknowledge;
+    sc_uint<16> StatusBits;
+    sc_uint<16> WindowSize;
+    sc_uint<16> Checksum;
+    sc_uint<16> UrgentPointer;
+    char Data[DATA_SIZE];
+};
+
+std::ostream& operator<<(std::ostream& os, const TCPHeader& packet) {
+    os << "TCP[Seq=" << packet.SequenceNumber
+        << ", Src=" << packet.SourcePort
+        << ", Dst=" << packet.DestinationPort << "]";
+    return os;
+}
+
+SC_MODULE(Producer) {
+    // Multi-port output, can connect to multiple FIFOs
+    sc_port<sc_fifo_out_if<TCPHeader>, 0> out;
+
+    SC_CTOR(Producer) : sequence_number(1) {
+        SC_THREAD(producer_thread);
+    }
+
+private:
+    void producer_thread();
+    sc_uint<32> sequence_number;
+    std::mt19937 rng;
+    std::uniform_int_distribution<int> time_dist{ 2, 10 }; 
+};
+
+void Producer::producer_thread() {
+    rng.seed(static_cast<unsigned int>(std::chrono::steady_clock::now().time_since_epoch().count()));
+
+    while (true) {
+        int delay_ms = time_dist(rng);
+        wait(delay_ms, SC_MS);
+
+        TCPHeader packet = {};
+        packet.SourcePort = 8080;
+        packet.DestinationPort = 80;
+        packet.SequenceNumber = sequence_number++;
+        packet.Acknowledge = 0;
+        packet.StatusBits = 0x0018;
+        packet.WindowSize = 65535;
+        packet.Checksum = 0;
+        packet.UrgentPointer = 0;
+
+        for (int i = 0; i < DATA_SIZE; i++) {
+            packet.Data[i] = static_cast<char>('A' + (i % 26));
+        }
+
+        for (int i = 0; i < out.size(); i++) {
+            out[i]->write(packet);
+        }
+
+        std::cout << "Producer: Packet sent at " << sc_time_stamp()
+            << " with sequence number " << packet.SequenceNumber
+            << " to " << out.size() << " consumers" << std::endl;
+    }
+}
+
+SC_MODULE(Consumer) {
+    sc_fifo_in<TCPHeader> in_port;
+
+    SC_CTOR(Consumer) {
+        SC_THREAD(consumer_thread);
+    }
+
+private:
+    void consumer_thread();
+};
+
+void Consumer::consumer_thread() {
+    while (true) {
+        TCPHeader packet = in_port.read();
+
+        std::cout << name() << ": Received packet at " << sc_time_stamp()
+            << " with sequence number " << packet.SequenceNumber
+            << std::endl;
+    }
+}
+
+SC_MODULE(TopLevel) {
+    Producer producer;
+    Consumer consumer1;
+    Consumer consumer2;
+    sc_fifo<TCPHeader> fifo_channel1;
+    sc_fifo<TCPHeader> fifo_channel2;
+
+    SC_CTOR(TopLevel) :
+        producer("Producer"),
+        consumer1("Consumer1"),
+        consumer2("Consumer2"),
+        fifo_channel1("FIFO1", 16),
+        fifo_channel2("FIFO2", 16) {
+
+        producer.out(fifo_channel1);
+        producer.out(fifo_channel2);
+
+        consumer1.in_port(fifo_channel1);
+        consumer2.in_port(fifo_channel2);
+    }
+};
+
+int sc_main(int argc, char* argv[]) {
+    TopLevel top("TopLevel");
+    sc_start(100, SC_MS);
+
+    return 0;
+}
